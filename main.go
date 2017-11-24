67c0047e65014c4bf1aeb47e877068e30fc84c19
Eduard Urbach <e.urbach@gmail.com>
2017-11-24 22:22:07 +0100

    Basic functionality

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "flag"
-import "sync"
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"sync"
+)
 
 func main() {
 	flag.Parse()
-
 	wg := sync.WaitGroup{}
 
 	for _, arg := range flag.Args() {
@@ -23,4 +28,24 @@ func main() {
 
 func get(repository string) {
 	fmt.Println(repository)
+	err := clone(repository)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func clone(repository string) error {
+	destination := path.Join(os.Getenv("GOPATH"), "src", repository)
+	fmt.Println(destination)
+	err := os.MkdirAll(destination, 0777)
+
+	if err != nil {
+		return err
+	}
+
+	url := "git@" + strings.Replace(repository, "github.com/", "github.com:", 1) + ".git"
+	cmd := exec.Command("git", "clone", "--depth", "1", url, destination)
+	fmt.Println(cmd.Args)
+	return cmd.Run()
 }
